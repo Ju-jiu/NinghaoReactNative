8c5858ad8253b8efb932d50b063edaf79714494d
wanghao <117663444@qq.com>
2016-01-07 13:21:32 +0800

    Switch：恢复到原始状态

--- app/Components/SwitchDemo.js
+++ app/Components/SwitchDemo.js
@@ -5,44 +5,17 @@ import React from 'react-native';
 let {
   Text,
   View,
-  Switch,
 } = React;
 
 class SwitchDemo extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      switchDemoOne: true,
-      switchDemoTwo: false,
-      switchDemoTwoDisabled: true,
-    }
   }
 
   render() {
     return (
-      <View style={{backgroundColor: '#fff', padding: 30}}>
-        <Switch
-          style={{marginBottom: 30}}
-          onTintColor="#6435c9"
-          tintColor="#eae7ff"
-          // thumbTintColor="#19962a"
-          value={this.state.switchDemoOne}
-          onValueChange={(value) => {
-            this.setState({
-              switchDemoOne: value,
-              switchDemoTwoDisabled: this.state.switchDemoTwoDisabled ? false : true
-            })}
-          }
-        />
-        <Switch
-          disabled={this.state.switchDemoTwoDisabled}
-          onTintColor="#6435c9"
-          tintColor="#eae7ff"
-          // thumbTintColor="#19962a"
-          value={this.state.switchDemoTwo}
-          onValueChange={(value) => this.setState({switchDemoTwo: value})}
-        />
+      <View>
+        <Text>Switch</Text>
       </View>
     );
   }
