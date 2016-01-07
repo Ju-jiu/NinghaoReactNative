9ae7da9dc8084d2e4d781066fc0e3b49ae3e61d0
wanghao <117663444@qq.com>
2016-01-07 12:15:28 +0800

    SliderIOS：组件的样式

--- app/Components/SliderIOSDemo.js
+++ app/Components/SliderIOSDemo.js
@@ -5,7 +5,6 @@ import React from 'react-native';
 let {
   Text,
   View,
-  SliderIOS,
 } = React;
 
 class SliderIOSDemo extends React.Component {
@@ -20,15 +19,7 @@ class SliderIOSDemo extends React.Component {
   render() {
     return (
       <View>
-        <SliderIOS
-          minimumValue={0}
-          maximumValue={10}
-          value={this.state.sliderValue}
-          onValueChange={(value) => this.setState({sliderValue: value})}
-          step={1}
-          // disabled={true}
-        />
-        <Text>{this.state.sliderValue}</Text>
+        <Text>SliderIOS</Text>
       </View>
     );
   }
