7aa08b4cd9981ff8622d2ebf2e3c9f617ce75250
wanghao <117663444@qq.com>
2016-01-07 09:05:45 +0800

    DatePickerIOS：日期时间选择工具

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,12 @@
 /**
  * Ninghao.net
- * https://github.com/ninghao/ninghao-react-native-demo
+ * https://github.com/ninghao/NinghaoReactNative
  */
 'use strict';
 
 import React from 'react-native';
 import styles from './app/Styles/Main';
+import DatePickerIOSDemo from './app/Components/DatePickerIOSDemo';
 
 var {
   AppRegistry,
@@ -24,6 +25,7 @@ class NinghaoReactNative extends React.Component {
           padding: 30,
           paddingTop: 60,
       }]}>
+        <DatePickerIOSDemo />
       </View>
     );
   }
